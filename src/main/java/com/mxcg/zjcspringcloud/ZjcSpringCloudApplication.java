fcc9bb472d6ecaee3d4f9a4ccf1ffa5dbfcfba72
邹劲程 <zjc@tofocus.cn>
2018-11-15 20:07:50 +0800

    Initial commit

---
+++ src/main/java/com/mxcg/zjcspringcloud/ZjcSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package com.mxcg.zjcspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjcSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjcSpringCloudApplication.class, args);
+    }
+}
